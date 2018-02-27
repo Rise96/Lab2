d54230f3b6424a0e13a28021d0a88acabe61f0be
Rise <ritm-temp@hotmail.com>
2018-02-27 10:13:08 +0600

    Lab2V1

---
+++ Lab2/Lab2/LettersFinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2
+{
+    class LettersFinder
+    {
+        public int FindALetter(String inStr)
+        {
+            int result=0;
+            int numOfLetInWord=0;
+            for(int i = 0; i < inStr.Length; i++)
+            {
+                if(inStr[i]=='A')
+                {
+                    numOfLetInWord++;
+                }
+                if(inStr[i]==' ')
+                {
+                    numOfLetInWord=0;
+                }
+                if (numOfLetInWord == 3)
+                {
+                    numOfLetInWord = 0;
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inStr = Console.ReadLine();
+            LettersFinder findALetter = new LettersFinder();
+            int numberOfWords = findALetter.FindALetter(inStr);
+            Console.WriteLine($"Число слов, в которых встретилось три буквы А, равно {numberOfWords}");
+            Console.ReadLine();
+        }
+    }
+}
